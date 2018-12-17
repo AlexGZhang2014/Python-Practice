27c3e28f697d250a16ddca0593e303bfe1042aa1
Alex Zhang <alexgzhang92@gmail.com>
2018-12-16 20:33:07 -0500

    Update

--- Python 2 Practice Problems.py
+++ Python 2 Practice Problems.py
@@ -21,3 +21,14 @@ def is_int(x):
     return True
   else:
     return False
+
+3. Write a function called digit_sum that takes a positive integer n as input and returns the sum of all that number's digits.
+For example: digit_sum(1234) should return 10 which is 1 + 2 + 3 + 4.
+(Assume that the number you are given will always be positive.)
+
+def digit_sum(n):
+  total = 0
+  str_num = str(n)
+  for num in str_num:
+    total += int(num)
+  return total
