ca6216786f9ad84fa42ba535e55a18784c379fa0
Alex Zhang <alexgzhang92@gmail.com>
2018-12-17 21:55:29 -0500

    Update

--- Python 2 Practice Problems.py
+++ Python 2 Practice Problems.py
@@ -79,3 +79,23 @@ def anti_vowel(text):
 
 print(anti_vowel("Hey You!"))
 print(anti_vowel("Hey look, words!"))
+
+8. Define a function scrabble_score that takes a string word as input and returns the equivalent scrabble score for that word.
+
+Assume your input is only one word containing no spaces or punctuation.
+As mentioned, no need to worry about score multipliers!
+Your function should work even if the letters you get are uppercase, lowercase, or a mix.
+Assume that you're only given non-empty strings.
+                                                                                                              
+score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2, 
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3, 
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1, 
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4, 
+         "x": 8, "z": 10}
+
+def scrabble_score(word):
+  total = 0
+  lower_word = word.lower()
+  for letter in lower_word:
+    total += score[letter]
+  return total                                                                                                           
